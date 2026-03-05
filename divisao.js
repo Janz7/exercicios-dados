1c6cfaf7aa969a7c1336b5ff434700f66a2bd7b6
Eduardo Dominicini Janz <dudujanz@gmail.com>
2026-03-05 12:08:11 -0300

    Removendo testes

--- divisao.js
+++
@@ -1,6 +0,0 @@
-let num1 = 10;
-let num2 = 100;
-
-let div = num1 / num2;
-
-console.log(`Divisão = ${div}`);
--- soma_tres_num.js
+++
@@ -1,7 +0,0 @@
-let num1 = 12;
-let num2 = 21;
-let num3 = 22;
-
-let soma = num1 + num2 + num3;
-
-console.log(`Soma = ${soma}`);
